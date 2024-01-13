c0d0ef8b4d8443382e0f3556c5c7d56068435500
nataliahanderek <72739947+nataliahanderek@users.noreply.github.com>
2024-01-13 14:49:52 +0100

    adding worked OpenMP

--- sitoOpenMP.c
+++ sitoOpenMP.c
@@ -1,67 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
+#include <omp.h>
 #include <time.h>
 
 // gcc -fopenmp sitoOpenMP.c -o sito
 // ./sito
 
+// Funkcja realizująca sito Eratostenesa dla liczb nieparzystych
+int eratosthenesOdd(int lastNumber, bool useOpenMP) {
+    // Włączanie/wyłączanie OpenMP
+    omp_set_num_threads(useOpenMP ? omp_get_num_procs() : 1);
 
-void sitoEratostenesaOpenMP(int n, bool prime[]) {
-    #pragma omp parallel for num_threads(4)
-    for (int i = 0; i <= n; i++) {
-        prime[i] = true;
-    }
+    // Ograniczenie warunku w pętli dla optymalizacji OpenMP
+    const int lastNumberSqrt = (int)sqrt((double)lastNumber);
+    int memorySize = (lastNumber - 1) / 2;
 
-    // Algorytm sita Eratostenesa
-    #pragma omp parallel num_threads(4)
-    for (int p = 2; p * p <= n; p++) {
-        if (prime[p] == true) {
-            bool local_prime_p = prime[p];
-            #pragma omp for
-            for (int i = p * p; i <= n; i += p) {
-                prime[i] = false;
-            }
-        }
-    }
+    // Inicjalizacja tablicy isPrime, reprezentującej liczby pierwsze
+    char* isPrime = (char*)malloc((memorySize + 1) * sizeof(char));
+
+    // Ustawienie wszystkich wartości na 1 (liczba pierwsza)
+    #pragma omp parallel for
+    for (int i = 0; i <= memorySize; i++)
+        isPrime[i] = 1;
+
+    // Wykreślanie liczb nieparzystych, które nie są pierwsze
+    #pragma omp parallel for schedule(dynamic)
+    for (int i = 3; i <= lastNumberSqrt; i += 2)
+        if (isPrime[i / 2])
+            for (int j = i * i; j <= lastNumber; j += 2 * i)
+                isPrime[j / 2] = 0;
+
+    // Obliczanie liczby liczb pierwszych
+    int found = lastNumber >= 2 ? 1 : 0;
+
+    #pragma omp parallel for reduction(+:found)
+    for (int i = 1; i <= memorySize; i++)
+        found += isPrime[i];
+
+    // Zwolnienie zaalokowanej pamięci
+    free(isPrime);
+    return found;
 }
 
-void printSito(int n, bool prime[]) {
-    printf("Liczby pierwsze do %d:\n", n);
-    for (int p = 2; p <= n; p++) {
-        if (prime[p]) {
-            printf("%d ", p);
-        }
-    }
-    printf("\n");
-}
+int main() {
+    int lastNumber = 100;
+    bool useOpenMP = true;
 
-int main(int argc, char *argv[]) {
-    int values[] = {300000, 500000, 1000000, 1500000, 2000000, 50000000};
+    clock_t start, end;
+    double cpu_time_used;
 
-    printf("\n\nOpenMP\n--------------------------------------------\n");
+    // Pomiar czasu rozpoczyna się
+    start = clock();
+    // Wywołanie funkcji
+    int primeCount = eratosthenesOdd(lastNumber, useOpenMP);
+    // Pomiar czasu kończy się
+    end = clock();
 
-    for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++) {
-        double cpu_time_used;
-        clock_t start = 0, end = 0;
+    // Obliczenie czasu wykonywania programu w sekundach
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-        bool *prime = (bool *)malloc((values[i] + 1) * sizeof(bool));
-        if (prime == NULL) {
-            fprintf(stderr, "Błąd alokacji pamięci.\n");
-            return 1;
-        }
-
-        start = clock();
-        sitoEratostenesaOpenMP(values[i], prime);
-        end = clock();
-
-        //printSito(values[i], prime);
-
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-        printf("Czas wykonania dla n = %d: %f sekundy\n", values[i], cpu_time_used);
-
-        free(prime);
-       }
+    // Wyświetlenie wyników
+    printf("Liczba liczb pierwszych mniejszych lub równych %d: %d\n", lastNumber, primeCount);
+    printf("Czas wykonania: %f sekundy\n", cpu_time_used);
 
     return 0;
 }
